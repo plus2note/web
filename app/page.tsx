6042e6448b21c01d6d363432adea4d8af804a8f0
Pradip Chaudhary <chaudharypradip678@gmail.com>
2024-03-21 07:13:32 +0545

    refactor: Convert WordPress app to Next.js

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+   return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+         <h1 className="text-2xl">Plus2note</h1>
+      </main>
+   );
+}
