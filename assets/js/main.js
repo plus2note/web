c32f4476e37495d4d3d8aca537dbbebbf40c165d
Pradip Chaudhary <chaudharypradip678@gmail.com>
2024-03-21 12:24:03 +0545

    feat(theme): Convert WordPress theme to Next.js

---
+++ assets/js/main.js
@@ -0,0 +1,1 @@
+console.log("Welcome to the Plus2note !")
---
+++ header.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Header template.
+ * 
+ * @package Plus2note
+ * 
+ */
+
+?>
+
+<!DOCTYPE html>
+<html lang="<?php echo language_attributes(); ?>">
+
+<head>
+    <meta charset="<?php echo bloginfo('charset') ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Plus (+2) Note </title>
+    <?php wp_head(); ?>
+
+</head>
+
+<body>
